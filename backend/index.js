8b5fe4149fe504a0b6012a32e10fdec5d50d7625
Gajanan Patil <gajananpatil7491@gmail.com>
2026-06-13 09:38:51 +0530

    solve connecting issue regarding userRoute in index.js file. everything is working post request sending proper response on http://localhost:5000/api/users/register

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import connectDB from "./src/config/db.js";
+import userRoute from "./src/routes/userRoute.js";
 
 dotenv.config();
 
@@ -13,13 +14,15 @@ await connectDB();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/users", userRoute);
+
 app.get("/", (req, res) => {
   res.json({
     message: "Myntra Clone API Running",
   });
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server Running on Port ${PORT}`);
