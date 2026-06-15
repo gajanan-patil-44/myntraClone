761a646ef8eb89b522062ee41db726beeff88bef
Gajanan Patil <gajananpatil7491@gmail.com>
2026-06-15 19:42:26 +0530

    added createProduct and respective rout which only access through admin credential normal user get block

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -45,3 +45,70 @@ export const getProductById = async (req, res) => {
     });
   }
 };
+
+// admin routes for create, update, delete products will be implemented in adminController.js
+export const createProduct = async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      category,
+      subCategory,
+      brand,
+      price,
+      discountPrice,
+      stock,
+      sizes,
+      colors,
+      images,
+      isActive,
+    } = req.body;
+
+    // Required fields validation
+    if (
+      !name ||
+      !description ||
+      !category ||
+      !subCategory ||
+      !brand ||
+      price === undefined ||
+      price < 0 ||
+      stock === undefined ||
+      stock < 0 ||
+      !images ||
+      !Array.isArray(images) ||
+      images.length === 0
+    ) {
+      return res.status(400).json({
+        success: false,
+        message: "All required fields are mandatory.",
+      });
+    }
+
+    const product = await Product.create({
+      name,
+      description,
+      category,
+      subCategory,
+      brand,
+      price,
+      discountPrice,
+      stock,
+      sizes,
+      colors,
+      images,
+      isActive,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Product created successfully.",
+      product,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- backend/src/routes/productRoute.js
+++ backend/src/routes/productRoute.js
@@ -1,11 +1,14 @@
 import express from "express";
 
-import { getAllProducts, getProductById } from "../controllers/productController.js";
+import { getAllProducts, getProductById, createProduct } from "../controllers/productController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
+import adminMiddleware from "../middleware/adminMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", getAllProducts);
 router.get("/:id", getProductById);
+router.post("/",authMiddleware, adminMiddleware ,createProduct); // This route protected and only accessible by admin.
 
 
 export default router;
