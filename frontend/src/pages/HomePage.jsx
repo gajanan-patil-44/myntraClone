289f5a7c505055b5cb27f2bb37222ac71b146254
Gajanan Patil <gajananpatil7491@gmail.com>
2026-06-21 19:15:17 +0530

    feat-implement Shop by Category

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import bannerWomenSale from "../assets/banners/banner-women-sale.jpg";
 import bannerElectronicsSale from "../assets/banners/banner-electronics-sale.jpg";
 
 import categoryPlaceholder from "../assets/categories/category-placeholder.jpg";
+import { categories } from "../constants/categories";
 
 const HomePage = () => {
   const banners = [
@@ -22,7 +23,14 @@ const HomePage = () => {
     },
   ];
 
-  
+  const shopCategories = Object.entries(categories).flatMap(
+  ([category, subcategories]) =>
+    subcategories.map((subcategory) => ({
+      category,
+      subcategory,
+      image: categoryPlaceholder,
+    }))
+);
 
   const [currentSlide, setCurrentSlide] = useState(0);
 
@@ -64,6 +72,44 @@ const HomePage = () => {
           ))}
         </div>
       </section>
+
+      {/* Shop by Category */}
+      <section className="max-w-7xl mx-auto px-4 py-12">
+  <h2 className="text-3xl font-bold text-center mb-10">
+    Shop By Category
+  </h2>
+
+  <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
+    {shopCategories.map((item) => (
+      <div
+        key={`${item.category}-${item.subcategory}`}
+        className="cursor-pointer group"
+        onClick={() => {
+          // TODO:
+          // navigate(
+          // `/products?category=${item.category}&subcategory=${item.subcategory}`
+          // );
+        }}
+      >
+        <div className="overflow-hidden rounded-lg border-[5px] border-solid border-orange-300">
+          <img
+            src={item.image}
+            alt={item.subcategory}
+            className="w-full aspect-square object-cover group-hover:scale-105 transition duration-300"
+          />
+        <div className="my-3 text-center">
+          <p className="font-semibold">
+            {item.subcategory}
+
+          </p>
+
+        </div>
+        </div>
+
+      </div>
+    ))}
+  </div>
+</section>
     </>
   );
 };
