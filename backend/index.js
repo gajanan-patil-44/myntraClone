46359b137a95c19c67e30ed5853a78009643c8fd
Gajanan Patil <gajananpatil7491@gmail.com>
2026-06-21 22:37:34 +0530

    add origin in cors at backend server

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,11 @@ const app = express();
 
 await connectDB();
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}
+));
 app.use(express.json());
 
 app.use("/api/users", userRoute);
